0776d2df829bfabdee8602bb817e34d2a930e5e3
cuterwrite <158505862@qq.com>
2024-06-20 00:38:06 +0800

    docs: ✏️ Added new post hugo-pwa

---
+++ sw.js
@@ -0,0 +1,132 @@
+importScripts('https://cdn.bootcdn.net/ajax/libs/workbox-sw/7.1.0/workbox-sw.js');
+
+// 缓存版本号
+let cacheVersion = '-240619';
+// 最大条目数
+const maxEntries = 100;
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+    // 评论缓存
+    workbox.routing.registerRoute(
+        new RegExp('^https://comment\.cuterwrite\.top'),
+        new workbox.strategies.NetworkFirst({
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxEntries: maxEntries,
+                    maxAgeSeconds: 30 * 24 * 60 * 60,
+                }),
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        })
+    );
+    // rss 、sitemap 不缓存
+    workbox.routing.registerRoute(
+        new RegExp('^https://cuterwrite\.top/(index|sitemap)\.xml'),
+        new workbox.strategies.NetworkOnly()
+    );
+    // 缓存 HTML
+    workbox.routing.registerRoute(
+        new RegExp('.*\.html'),
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'html-cache' + cacheVersion,
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxEntries: maxEntries,
+                    maxAgeSeconds: 30 * 24 * 60 * 60,
+                }),
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        })
+    );
+    // 缓存 Google Fonts
+    workbox.routing.registerRoute(
+        new RegExp('^https://fonts\.font\.im'),
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'google-fonts' + cacheVersion,
+            plugins: [
+                // 使用 expiration 插件实现缓存条目数目和时间控制
+                new workbox.expiration.ExpirationPlugin({
+                    // 最大缓存条目数
+                    maxEntries: maxEntries,
+                    // 最长缓存时间 30 天
+                    maxAgeSeconds: 30 * 24 * 60 * 60,
+                }),
+                // 使用 cacheableResponse 插件缓存状态码为 0 的请求
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        })
+    );
+    // 缓存 bootcdn、unpkg、jsdelivr 等公共库，用正则匹配
+    workbox.routing.registerRoute(
+        new RegExp('^https?://(?:cdn\.bootcdn\.net|unpkg\.com|cdn\.jsdelivr\.net)'),
+        new workbox.strategies.CacheFirst({
+            cacheName: 'bootcdn' + cacheVersion,
+            fetchOptions: {
+                mode: 'cors',
+                credentials: 'omit',
+            },
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxEntries: maxEntries,
+                    maxAgeSeconds: 30 * 24 * 60 * 60,
+                }),
+            ],
+        })
+    );
+    // 自建 UMA 统计脚本: https://analytics.cuterwrite.top/uma
+    workbox.routing.registerRoute(
+        new RegExp('^https://analytics\.cuterwrite\.top/uma'),
+        new workbox.strategies.NetworkOnly({
+            plugins: [
+                // 使用 background sync 插件实现后台同步
+                new workbox.backgroundSync.BackgroundSyncPlugin('Optical_Collect', {
+                    maxRetentionTime: 12 * 60,
+                }),
+            ],
+        })
+    );
+    // 缓存存储桶图片 https://cuterwrite-1302252842.file.myqcloud.com/
+    workbox.routing.registerRoute(
+        new RegExp('^(https://cuterwrite-1302252842\.file\.myqcloud\.com|https://s2\.loli\.net)'),
+        new workbox.strategies.CacheFirst({
+            cacheName: 'image-cache' + cacheVersion,
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxEntries: maxEntries,
+                    maxAgeSeconds: 30 * 24 * 60 * 60,
+                }),
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        })
+    );
+
+    // 后缀匹配，针对其余没有被域名匹配到的静态文件
+    workbox.routing.registerRoute(
+        new RegExp('.*.(?:png|jpg|jpeg|svg|gif|webp)'),
+        new workbox.strategies.StaleWhileRevalidate()
+    );
+    workbox.routing.registerRoute(
+        new RegExp('.*.(css|js)'),
+        new workbox.strategies.StaleWhileRevalidate()
+    );
+
+    // 默认匹配剩下的请求
+    workbox.routing.setDefaultHandler(
+        // 优先使用缓存，缓存没有则使用网络请求
+        new workbox.strategies.NetworkFirst({
+            networkTimeoutSeconds: 3,
+        })
+    );
+
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
